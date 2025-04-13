c42f7f4d260d440daff6c32ebca46262343e066d
f-fida-coder <numericfida786@gmail.com>
2025-04-13 14:28:52 +0500

    new Update 1- banner

--- softivuspro.com/oddsx/oddsx/assets/js/main.js
+++ softivuspro.com/oddsx/oddsx/assets/js/main.js
@@ -446,3 +446,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   });
 });
+
+// slider code
+
+const slider = document.getElementById("slider");
+    const slides = slider.querySelectorAll("img");
+    let index = 0;
+
+    document.getElementById("next").addEventListener("click", () => {
+      index = (index + 1) % slides.length;
+      slider.style.transform = `translateX(-${index * 100}%)`;
+    });
+
+    document.getElementById("prev").addEventListener("click", () => {
+      index = (index - 1 + slides.length) % slides.length;
+      slider.style.transform = `translateX(-${index * 100}%)`;
+    });
